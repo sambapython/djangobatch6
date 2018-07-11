b2f026d31bff11344a7dfb76e2e63cc6e06d556c
sambapython <sambapython@gmail.com>
2018-07-11 10:19:56 +0530

    2018-07-11 models with relations.

--- sample_model/app1/models.py
+++ sample_model/app1/models.py
@@ -10,5 +10,24 @@ class Product(models.Model):
 	cost=models.IntegerField()
 
 	class Meta:
-		db_table="prouct"
+		db_table="product"
+class Customer(models.Model):
+	name=models.CharField(max_length=250)
+	address = models.TextField()
+
+	class Meta:
+		db_table="customer"
+class SalesOrder(models.Model):
+	salesorder_types=[('online',"Online payment"),("cod","CASH ON DELIVERY")]
+	name=models.CharField(max_length=250)
+	product = models.ManyToManyField(Product)
+	customer=models.ForeignKey(Customer)
+	sal_type=models.CharField(choices=salesorder_types, max_length=6)
+
+	class Meta:
+		db_table="salesorders"
+
+
+
+
 
