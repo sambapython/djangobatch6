f2c8dccadff5a234a40854980400a581b8761040
sambapython <sambapython@gmail.com>
2018-07-13 10:10:19 +0530

    2018-07-13. temlates with model data.

--- sample_model/app1/views.py
+++ sample_model/app1/views.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from app1.models import Customer	
 
 # Create your views here.
+def sales(request):
+	return render(request,"app1/sales.html")
+def products(request):
+	return render(request,"app1/product.html")
+def customers(request):
+	#return HttpResponse("customers")
+	'''
+	list_customer = ["anil","ashok","pavan","aleka"]
+	message="showing customers successfully"
+	return render(request,"app1/customer.html",{"data":list_customer,
+		"msg": message})
+	'''
+	customers = Customer.objects.all()
+	return render(request,"app1/customer.html",{"data":customers})
+
--- sample_model/sample_model/urls.py
+++ sample_model/sample_model/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
+from app1.views import sales, products, customers
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^customers/', customers),
+    url(r'^products/', products),
+    url(r'^sales/', sales),
+
 ]
